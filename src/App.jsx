b621a2492058de461cd422a0ddb849bbdc271b9a
Ajay Mayani <ajaymayani96@gmail.com>
2025-03-09 00:40:48 +0530

    update contact import statement

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import About from "./components/About";
 import Header from "./components/Header";
 import Projects from "./components/Projects";
 import Testimonails from "./components/Testimonails";
-import Contact from "./components/contact";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 function App() {
